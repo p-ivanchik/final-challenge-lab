ff0b4d520dc42cdd631e64d1e43d90d2de92fbe8
PavloIvanchykTR <pavel.ivanchik@thomsonreuters.com>
2022-02-15 10:00:30 +0200

    Set env. vars

--- 0.init-env.sh
+++ 0.init-env.sh
@@ -4,4 +4,3 @@
 
 git config --global user.email "${account}"
 git config --global user.name "User Name"
-git remote add google ssh://${USER_ID}@source.developers.google.com:2022/p/${project}/r/${REPO_NAME}
